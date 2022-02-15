ac2a75bdad7834b65a53eb53cadbdac622108f11
Tuyen LIeu <lieuthimongtuyen@gmail.com>
2022-02-15 16:40:11 -0500

    only add comment to select by value

--- src/SelectDropDown.java
+++ src/SelectDropDown.java
@@ -7,27 +7,29 @@ public class SelectDropDown {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","C:\\Browser Drivers\\Chromedriver_98.0.478.80.exe");
-		
+
 		ChromeDriver driver = new ChromeDriver();
-		
+
 		driver.get("https://www.sugarcrm.com/request-demo/");
-		
+
 		driver.manage().window().maximize();
-		
+
 		/*select value from drop downlist = company size */
 		WebElement dropdown = driver.findElement(By.name("employees_c"));
-		
+
 		Select select = new Select(dropdown);
-		
+
 		/*select by index*/
 		select.selectByIndex(2);
 		Thread.sleep(2000);
-		
+
+
+	  /*select by value*/
 		select.selectByValue("level10");
 		Thread.sleep(2000);
-		
+
 		driver.findElement(By.xpath("//*[@id=\"CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll\"]")).click();
-		
+
 		driver.close();
 	}
 
